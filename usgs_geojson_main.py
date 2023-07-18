57d5729d3976231a02261582d811476ed37bd209
LordBramster <49885100+LordBramster@users.noreply.github.com>
2023-07-18 16:50:20 -0600

    Added multiple webhook URLs feature, and renamed temp config.

--- usgs_geojson_main.py
+++ usgs_geojson_main.py
@@ -1,4 +1,6 @@
 import os
+from datetime import datetime
+import json
 import usgs_geojson_api as usgs_api
 import usgs_geojson_webhook as usgs_webhook
 
@@ -18,20 +20,37 @@ if __name__ == "__main__":
     quake_list, quake_str = usgs_api.geojson_get_quakes(api_raw)
 
     # USGS WEBHOOK
-    webhook_msg = ''
-    # TODO replace input webhook URL to sanitize for public server/job use.
-    webhook_url = os.environ['ENV_URL']  # usgs_webhook.get_webhook_url()
+    webhook_datetime = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    webhook_msg = f'`{webhook_datetime} | <{feed_magnitude}><{feed_interval}>`'
+    # webhook_url = os.environ['ENV_URL'] or use config.json's -> usgs_webhook.get_webhook_url()
+    webhook_urls = json.loads(os.environ['ENV_URLS'])  # '["URL1", "URL2", "URL3", "URL+"]'
     webhook_username = usgs_webhook.get_webhook_username()
     webhook_embeds_color = usgs_webhook.get_webhook_embeds_color('violet')
     webhook_embeds = [
         {
-            "title": f"Earthquakes :: <{feed_magnitude}><{feed_interval}>",
+            "title": f"Earthquakes",
             "description": quake_str,
             "color": webhook_embeds_color
         }
     ]
-    webhook_status, webhook_sent = usgs_webhook.send_webhook(webhook_username, webhook_msg, webhook_embeds, webhook_url)
+
+    # FOR ALL URLs, SEND SAME WEBHOOK MESSAGE
+    for url in webhook_urls:
+        # SEND WEBHOOK
+        webhook_status, webhook_sent = usgs_webhook.send_webhook(
+            webhook_username,
+            webhook_msg,
+            webhook_embeds,
+            url
+        )
+        # Print
+        print(f"\nWEBHOOK: {webhook_sent}")
+        print(f"RESPONSE: {webhook_status}")
+
+    # TODO send multiple webhooks at once, and update webhook_config to have multiple keys.
+    # SEND WEBHOOK
+    # webhook_status, webhook_sent = usgs_webhook.send_webhook(webhook_username, webhook_msg, webhook_embeds, webhook_url)
 
     # Print
-    print(f"\nWEBHOOK: {webhook_sent}")
-    print(f"RESPONSE: {webhook_status}")
+    # print(f"\nWEBHOOK: {webhook_sent}")
+    # print(f"RESPONSE: {webhook_status}")
